8a44eb655c0a9f1d6512bf21a9abdb35e3aee80a
MelihOZKAS <melihozkass@gmail.com>
2024-07-05 15:41:06 +0300

    ALLAH ım utandırma RABBİM

--- isimler/views.py
+++ isimler/views.py
@@ -477,10 +477,10 @@ def aiadd(request):
         Post_Turu_Gelen = PostKategori.objects.get(short_title=Post_Turu)
 
         yeni_Slug = create_unique_title_slug(slug)
-        isimekle = Post(title=title, slug=yeni_Slug, h1=h1, isim=isim,kisaanlam=kisaaciklama, Post_Turu=Post_Turu_Gelen, icerik1=icerik1, icerik2=icerik2)
+        isimekle = Post(title=title, slug=yeni_Slug, h1=h1, isim=isim, kisaanlam=kisaaciklama, Post_Turu=Post_Turu_Gelen, icerik1=icerik1, icerik2=icerik2)
         isimekle.save()
 
-        allname.objects.filter(id=GelenID).update(Durum="Tamamlandi")
+        allname.objects.filter(id=GelenID).update(Durum="Tamamlandı")
 
         if isimekle.id is None:
             return HttpResponse("Post kaydedilemedi.")
